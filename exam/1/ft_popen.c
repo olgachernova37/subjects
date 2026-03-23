6bbf460a238ff7b99f4f4b38e82725e9cfdffe53
Olha Demianyk <olcherno@c1r3s5.42prague.com>
2026-03-23 17:27:38 +0100

    asdsda

--- exam/1/ft_popen.c
+++ exam/1/ft_popen.c
@@ -3,11 +3,10 @@
 
 int ft_popen(const char *file, char *const av[], char type)
 {
-   int fd[2];
-   pid_t pid;
+    int fd[2];
+    pid_t pid;
 
-   if (!file || !av || (type != 'r')&& (type != 'w') || pipe(fd) == -1)
-    return (-1);
+    if(!file || !av || (type != 'r' && type != 'w') || pipe(fd) == -1)
+        return(-1);
     
-
 }
