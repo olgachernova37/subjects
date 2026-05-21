af40500744f912b05796eec3c6d5ad742cea84d1
Olha Demianyk <olcherno@c1r6s4.42prague.com>
2026-05-21 15:54:46 +0200

    ex00new

--- solutions/C++00/ex00/megaphone.cpp
+++ solutions/C++00/ex00/megaphone.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
 #include <cctype> 
-using namespace std;
+#include <string>
 
 int main(int argc, char** argv)
 {
      //std::cout << "debug";
     if (argc >= 2)
     {
-        int i =0;
-        int j =1;
-        while(j<argc)
+      for (int i=1; i<argc; i++)
+      {
+        std::string slovo = argv[i];
+        for (size_t j=0; j<slovo.length(); j++)
         {
-           
-            while(argv[j][i] != '\0')
-            {
-                if(islower(argv[j][i]))
-                {
-                    argv[j][i] = (char)toupper(argv[j][i]);
-                }
-                i++;
-            }  
-            i=0;
-            std::cout << argv[j];
-            std::cout << " ";
-            j++;   
-        }    
+            slovo[j]= std::toupper(slovo[j]);
+
+        }
+        std::cout <<slovo;
+      }
     }
     else
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
