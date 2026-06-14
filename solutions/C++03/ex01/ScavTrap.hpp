b40265c7ba30d5a6f4bfb7d2831be2bbadcffe32
Olha <32342614+olgachernova37@users.noreply.github.com>
2026-06-14 11:58:25 +0000

    asd

--- solutions/C++03/ex01/ScavTrap.hpp
+++ solutions/C++03/ex01/ScavTrap.hpp
@@ -1,5 +1,14 @@
 #ifndef SCAVTRAP_HPP
 #define SCAVTRAP_HPP
 
-#include "ClapTrap.h"
+#include "ClapTrap.hpp"
 
+class ScavTrap : public ClapTrap {
+public:
+	ScavTrap(std::string name);
+	~ScavTrap();
+	void attack(const std::string& target);
+	void guardGate();
+};
+
+#endif
--- solutions/C++03/ex01/main.cpp
+++ solutions/C++03/ex01/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main(void) {
 	ClapTrap clapTrap("ClapTrap1");
@@ -7,6 +8,17 @@ int main(void) {
 	clapTrap.takeDamage(10);
 	clapTrap.beRepaired(5);
 	std::cout << "--------------------------------" << std::endl;
+	{
+		ScavTrap scavTrap("ScavTrap1");
+		scavTrap.attack("Enemy");
+		scavTrap.takeDamage(20);
+		scavTrap.beRepaired(15);
+		scavTrap.guardGate();
+	}
+	std::cout << "--------------------------------" << std::endl;
+	ScavTrap scavTrap2("ScavTrap2");
+	scavTrap2.attack("Enemy");
+	scavTrap2.guardGate();
 	ClapTrap clapTrap2("ClapTrap2");
 	clapTrap2.attack("Enemy");
 	clapTrap2.takeDamage(10);
